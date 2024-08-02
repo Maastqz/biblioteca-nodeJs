e3085e78ed12036a7d6f7034d77b0cc6d4b2deaa
Maastqz <127257996+Maastqz@users.noreply.github.com>
2024-08-02 19:30:03 -0300

    Add files via upload

--- src/cli.js
+++ src/cli.js
@@ -1,46 +1,36 @@
 import chalk from 'chalk';
 import fs from 'fs';
 import pegaArquivo from './index.js';
-import listaValidada from './http-validacao.js';
 
 const caminho = process.argv;
 
-async function imprimeLista(valida, resultado, identificador = '') {
-  if (valida) {
+function imprimeLista(resultado, identificador = '') {
     console.log(
-      chalk.yellow('lista validada'),
-      chalk.black.bgGreen(identificador),
-      await listaValidada(resultado));    
-  } else {
-    console.log(
-      chalk.yellow('lista de links'),
-      chalk.black.bgGreen(identificador),
+      chalk.yellow('link na lista'),
+      chalk.black.bgGreen(identificador), 
       resultado);
   }
-}
-
 
 async function processaTexto(argumentos) {
   const caminho = argumentos[2];
-  const valida = argumentos[3] === '--valida';
 
   try {
     fs.lstatSync(caminho);
   } catch (erro) {
     if (erro.code === 'ENOENT') {
-      console.log('arquivo ou diretório não existe');
+      console.log('arquivo ou diretório não existem');
       return;
     }
   }
 
   if (fs.lstatSync(caminho).isFile()) {
-    const resultado = await pegaArquivo(argumentos[2]);
-    imprimeLista(valida, resultado);
+    const resultado = await PegaArquivo(argumentos[2]);
+    imprimeLista(resultado);
   } else if (fs.lstatSync(caminho).isDirectory()) {
     const arquivos = await fs.promises.readdir(caminho)
-    arquivos.forEach(async (nomeDeArquivo) => {
-      const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-      imprimeLista(valida, lista, nomeDeArquivo)
+    arquivos.forEach(async (nomeDoArquivo) => {
+      const lista = await PegaArquivo(`${caminho}/${nomeDoArquivo}`)
+      imprimeLista(lista, nomeDoArquivo)
     })
   }
 }
